4a870437bb8c7669e83bf9a4aeceabdc84488ffc
jk10274 <72466440+jk10274@users.noreply.github.com>
2021-04-25 10:20:02 +0200

    js

--- ask/main.js
+++ ask/main.js
@@ -1,4 +1,4 @@
-function setQuestions(id) {
+function setQuestion(id) {
     $.ajax({
         url: "questions.json",
         dataType: "json",
@@ -13,13 +13,24 @@ function setQuestions(id) {
 }
 
 function showCorrect(answer) {
-    document.getElementById('dot-answer-' + answer).classList.remove('visually-hidden');
+    document.getElementById('answer-dot-' + answer).classList.remove('visually-hidden');
+    document.getElementById('answer-container-' + answer).style.backgroundColor = 'rgb(39, 39, 39)';
 }
 
 function showWrong(answer, solution) {
-    document.getElementById('dot-answer-' + solution).classList.remove('visually-hidden');
-    document.getElementById('dot-answer-' + answer).classList.remove('visually-hidden');
-    document.getElementById('dot-answer-' + answer).style.backgroundColor = 'rgb(206, 84, 76)';
+    document.getElementById('answer-dot-' + solution).classList.remove('visually-hidden');
+    document.getElementById('answer-container-' + solution).style.backgroundColor = 'rgb(39, 39, 39)';
+    document.getElementById('answer-dot-' + answer).classList.remove('visually-hidden');
+    document.getElementById('answer-dot-' + answer).style.backgroundColor = 'rgb(206, 84, 76)';
+    /* document.querySelector('#answer-container-' + i).addEventListener('click', nextquestion()); */
+}
+
+function resetAnswerLayout() {
+    for (var i = 1; i <= 4; i++) {
+        document.getElementById('answer-dot-' + i).style.backgroundColor = 'rgb(56, 139, 105)';
+        document.getElementById('answer-dot-' + i).classList.add('visually-hidden');
+        document.getElementById('answer-container-' + i).style.backgroundColor = 'rgb(0, 0, 0)';
+    }
 }
     
 function correctAnswer(id, answer) {
@@ -30,17 +41,45 @@ function correctAnswer(id, answer) {
         console.log(result);
         if (answer == result[id].solution) {
             showCorrect(answer);
+            /* setTimeout(function(){ nextQuestion(id); }, 3000); */
         } else {
             showWrong(answer, result[id].solution);
+            /* setTimeout(function(){ nextQuestion(id); }, 3000); */
         }
 
     });
 }
 
-var rqid = Math.floor((Math.random() * 2) + 1); // Random Question Identifier
-
-setQuestions(rqid);
-
 function sendAnswer(answer) {
     correctAnswer(rqid, answer);
+}
+
+function nextQuestion(oldId) {
+    var storedArray = JSON.parse(Cookies.get('history'));
+    storedArray.push(oldId);
+    rqid = Math.floor((Math.random() * 5) + 1); // Random Question Identifier
+    while (storedArray.includes(rqid) == true) {
+        rqid = Math.floor((Math.random() * 5) + 1); // Random Question Identifier
+    }
+    resetAnswerLayout();
+    setQuestion(rqid);
+    alert(rqid);
+}
+
+var rqid = 0;
+
+if (Cookies.get('history') == null) {
+    console.log("Init Test: No cookies yet");
+    var cookieConfirm = confirm("Dürfen wir Cookies nutzen?");
+    if (cookieConfirm == true) {
+        var historyArray = [0];
+        Cookies.set('history', JSON.stringify(historyArray));
+    } else {
+        alert("Ohne Zustimmung ist die Webseite nicht funktionsfähig.");
+        window.location.href  = "https://apuem.com";
+    }
+} else {
+    console.log("Init Test" + Cookies.get('history'));
+    rqid = Math.floor((Math.random() * 5) + 1); // Random Question Identifier
+    setQuestion(rqid);
 }
